db3299cead4b9909b84d07fef24f68993b44eced
Samuel Nichols <Nichols_Samuel@outlook.com>
2022-08-24 13:48:23 -0500

    Finished Implementing EF, about to add Metadata to our Data layer.

--- SATScheduling.UI.MVC/Program.cs
+++ SATScheduling.UI.MVC/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SATScheduling.UI.MVC.Data;
+using SATScheduling.DATA.EF.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<SATContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
