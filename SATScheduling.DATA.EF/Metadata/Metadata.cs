5efcd006a91474a97b5895ecc1c0321eb9f8e6ef
Samuel Nichols <Nichols_Samuel@outlook.com>
2022-08-25 09:30:26 -0500

    Finished setting up MetaData

--- SATScheduling.DATA.EF/Metadata/Metadata.cs
+++ SATScheduling.DATA.EF/Metadata/Metadata.cs
@@ -15,12 +15,11 @@ namespace SATScheduling.DATA.EF.Models //.Metadata
         [Required(ErrorMessage = "Course Name is required")]
         [StringLength(50)]
         [Display(Name = "Course")]
-        public string CourseName { get; set; }
+        public string CourseName { get; set; } = null!;
 
         [Required(ErrorMessage = "Course Description is required")]
-        [StringLength(int.MaxValue)]
         [Display(Name = "Course Description")]
-        public string CourseDescription { get; set; }
+        public string CourseDescription { get; set; } = null!;
 
         [Required(ErrorMessage = "Credit hours is required")]
         [Display(Name = "Credit Hours")]
@@ -54,44 +53,111 @@ namespace SATScheduling.DATA.EF.Models //.Metadata
         [Required]
         public DateTime EnrollmentDate { get; set; }
     }
-    #endregion
-
-    #region SATContext
-    public class SATContextMetadata
-    {
-
-    }
-    #endregion
+    #endregion    
 
     #region ScheduledClass
     public class ScheduledClassMetadata
     {
+        public int ScheduledClassId { get; set; }
 
+        public int CourseId { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]//0:d => MM/dd/yyyy
+        [Display(Name = "Start Date")]
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]//0:d => MM/dd/yyyy
+        [Display(Name = "End Date")]
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "Instructor Name is required")]
+        [StringLength(40)]
+        [Display(Name = "Instructor Name")]
+        public string InstructorName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Location is required")]
+        [StringLength(20)]
+        [Display(Name = "Location")]
+        public string Location { get; set; } = null!;
+
+        public int SCSID { get; set; }
     }
     #endregion
 
     #region ScheduledClassStatus
     public class ScheduledClassStatusMetadata
     {
+        public int SCSID { get; set; }
+        //PK, doesnt need any further rules
 
+        [Required(ErrorMessage = "Scheduled Class Status is Required")]
+        [StringLength(50)]
+        [Display(Name = "Scheduled Class Status")]
+        public string SCSName { get; set; } = null!;
     }
     #endregion
 
     #region Student
     public class StudentMetadata
     {
+        public int StudentId { get; set; }
 
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(20)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(20)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = null!;
+
+        [StringLength(15)]
+        public string? Major { get; set; }
+
+        [StringLength(50)]
+        public string? Address { get; set; }
+
+        [StringLength(25)]
+        public string? City { get; set; }
+
+        [StringLength(2)]
+        public string? State { get; set; }
+
+        [StringLength(10)]
+        [Display(Name = "Zip Code")]
+        public string? ZipCode { get; set; }
+
+        [StringLength(13)]
+        public string? Phone { get; set; }
+
+        [StringLength(60)]
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [StringLength(100)]
+        [Display(Name = "Photo")]
+        public string? PhotoUrl { get; set; }
+
+        public int SSID { get; set; }
     }
     #endregion
 
     #region StudentStatus
     public class StudentStatusMetadata
     {
+        public int SSID { get; set; }
 
+        [StringLength(30)]
+        [Display(Name = "Student Status")]
+        [Required]
+        public string SSName { get; set; } = null!;
+
+        [StringLength(250)]
+        [Display(Name = "Student Status Description")]
+        public string? SSDescription { get; set; }
     }
     #endregion
-
-
-
-
 }
